bc52d4cb9b47b2b70fe562a578474773b72c27b9
Will Campbell <wibb1@comcast.net>
2021-10-20 19:57:08 -0400

    Draft Meals Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals/>
+      </main>
+    </>
+  );
 }
 
 export default App;
